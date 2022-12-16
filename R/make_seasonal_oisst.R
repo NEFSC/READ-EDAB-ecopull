ce7e5d3ad91a54ecb132efef015163ff2210a5c8
Abigail <abigail.tyrell@noaa.gov>
2022-12-16 11:36:37 -0500

    organize files, new workflow writeup

---
+++ R/make_seasonal_oisst.R
@@ -0,0 +1,49 @@
+
+make_seasonal_oisst <- function(rasterfile,
+                                type) {
+  dat <- raster::stack(rasterfile)
+  dat <- raster::rotate(dat)
+  out <- tibble::tibble()
+
+  for(i in c("GOM", "GB", "MAB")) {
+    for (j in c("winter", "spring", "summer", "fall")) {
+
+      day_first <- dplyr::case_when(j == "winter" ~ 1,
+                                    j == "spring" ~ 91,
+                                    j == "summer" ~ 182,
+                                    j == "fall" ~ 274)
+      day_last <- dplyr::case_when(j == "winter" ~ 90,
+                                   j == "spring" ~ 181,
+                                   j == "summer" ~ 273,
+                                   j == "fall" ~ 365)
+
+      this_dat <- raster::mask(dat[[day_first:day_last]],
+                               sf::as_Spatial(ecodata::epu_sf[ecodata::epu_sf$EPU == i,]))
+
+      # get seasonal mean for each raster cell
+      results <- raster::stackApply(this_dat,
+                                    indices = rep(1, raster::nlayers(this_dat)),
+                                    mean)
+
+      # mean all the raster cells to 1 value
+      if(type == "ltm") {
+        results <- tibble::tibble(EPU = i,
+                                  Season = j,
+                                  LTM = mean(results@data@values, na.rm = TRUE))
+      } else if(type == "annual") {
+        results <- tibble::tibble(EPU = i,
+                                  Season = j,
+                                  Mean_temp = mean(results@data@values, na.rm = TRUE),
+                                  Year = as.numeric(stringr::str_extract(rasterfile, "[:digit:]{4}")))
+      }
+      out <- rbind(out, results)
+    }
+  }
+  return(out)
+}
+# make_annual_means("C:/Users/abigail.tyrell/Documents/ecopull/data-raw/gridded/sst_data/test_1982.grd")
+# test <- lapply(c("C:/Users/abigail.tyrell/Documents/ecopull/data-raw/gridded/sst_data/test_1982.grd",
+#          "C:/Users/abigail.tyrell/Documents/ecopull/data-raw/gridded/sst_data/test_1983.grd"),
+#        FUN = make_annual_means)
+# data.table::rbindlist(test) %>%
+#   tibble::as_tibble()
--- _targets.R
+++ _targets.R
@@ -8,21 +8,33 @@ library(targets)
 # Run the R scripts in the R/ folder with your custom functions:
 tar_source()
 
+# download new year's sst data and save in folder
+# download from psl, for example:
+# https://downloads.psl.noaa.gov/Datasets/noaa.oisst.v2.highres/sst.day.mean.2021.v2.nc
+# test_2021 <- ecopull::nc_to_raster(nc = "2021.nc", varname = "sst")
+# raster::writeRaster(test_2021, filename = "data-raw/gridded/sst_data/test_2021.grd", overwrite = TRUE)
+# downloading the .nc from url in the script breaks the file
+
+# # download new ltm
+# test <- ecopull::nc_to_raster(nc = here::here("data-raw/gridded/ltm/sst.day.mean.ltm.1991-2020.nc"),
+#                               varname = "sst")
+# raster::writeRaster(test, filename = "data-raw/gridded/ltm/new_internet_ltm.grd",
+#                     overwrite = TRUE)
+
 list(
-
-  # download new year's sst data and save in folder
-  # download from psl, for example:
-  # https://downloads.psl.noaa.gov/Datasets/noaa.oisst.v2.highres/sst.day.mean.2021.v2.nc
-  # test_2021 <- ecopull::nc_to_raster(nc = "2021.nc", varname = "sst")
-  # raster::writeRaster(test_2021, filename = "data-raw/gridded/sst_data/test_2021.grd", overwrite = TRUE)
-
   # read in long term data
   tar_target(ltm_file,
              here::here("data-raw","gridded","ltm","internet_ltm.grd"),
              format = "file"),
+  tar_target(new_ltm_file,
+             here::here("data-raw","gridded","ltm","new_internet_ltm.grd"),
+             format = "file"),
   # calculate ltm for each region x season
   tar_target(ltms,
              make_seasonal_oisst(ltm_file,
+                                 type = "ltm")),
+  tar_target(new_ltms,
+             make_seasonal_oisst(new_ltm_file,
                                  type = "ltm")),
 
   # read in yearly data
@@ -38,7 +50,7 @@ list(
                       FUN = make_seasonal_oisst,
                       type = "annual") %>%
                data.table::rbindlist() %>%
-                 tibble::as_tibble()
+               tibble::as_tibble()
              }),
 
   # subtract ltm to get anomaly
@@ -50,5 +62,22 @@ list(
                  dplyr::mutate(Value = Mean_temp - LTM,
                                Var = paste(EPU, Season, "sst_anomaly", sep = "_")) %>%
                  dplyr::select(Year, EPU, Var, Value)
-             })
+             }),
+  tar_target(new_sst_anomaly,
+             {
+               dplyr::full_join(annual_ssts,
+                                new_ltms) %>%
+                 dplyr::filter(Year > 1981) %>%
+                 dplyr::mutate(Value = Mean_temp - LTM,
+                               Var = paste(EPU, Season, "sst_anomaly", sep = "_")) %>%
+                 dplyr::select(Year, EPU, Var, Value)
+             }),
+
+  # save data to package
+  tar_target(save_data,
+             usethis::use_data(sst_anomaly, overwrite = TRUE)),
+  tar_target(save_new_data,
+             usethis::use_data(new_sst_anomaly, overwrite = TRUE)
+
+  )
 )
