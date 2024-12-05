768dace69562cf5e86da88cf9c4e81f6d1d3cea3
Abby Tyrell <77738923+atyrell3@users.noreply.github.com>
2024-12-05 10:13:12 -0500

    add data documentation

---
+++ R/data.R
@@ -0,0 +1,23 @@
+#' @title SST anomaly using long-term mean from 1991-2020
+#' @description SST anomaly using long-term mean from 1991-2020
+#' @format A data frame with 513 rows and 4 variables:
+#' \describe{
+#'   \item{\code{Year}}{double Year}
+#'   \item{\code{EPU}}{character Ecological Production Unit}
+#'   \item{\code{Var}}{character Indicator name}
+#'   \item{\code{Value}}{double Anomaly value (degrees C)}
+#'}
+#' @details DETAILS
+"new_sst_anomaly"
+
+#' @title SST anomaly using long-term mean from 1982-2010
+#' @description SST anomaly using long-term mean from 1982-2010
+#' @format A data frame with 513 rows and 4 variables:
+#' \describe{
+#'   \item{\code{Year}}{double Year}
+#'   \item{\code{EPU}}{character Ecological Production Unit}
+#'   \item{\code{Var}}{character Indicator name}
+#'   \item{\code{Value}}{double Anomaly value (degrees C)}
+#'}
+#' @details DETAILS
+"sst_anomaly"
