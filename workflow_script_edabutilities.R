6025079ac08a9244edfe651e5ee9a4a8cd503a14
atyrell3 <abigail.tyrell@noaa.gov>
2026-08-17 12:36:43 -0500

    debug workflows script

--- workflow_script_edabutilities.R
+++ workflow_script_edabutilities.R
@@ -1,5 +1,8 @@
-#Command Line Cloud
-#Rscript //nefscdata/EDAB_Workflows/GLORYS_automation_test.R "//nefscdata/EDAB_Datasets/OISST/V2/SOURCE/SST" "//nefscdata/EDAB_Dev/atyrell"
+#Command Line Local
+#Rscript //nefscdata/EDAB_Workflows/GLORYS_automation_test.R "//nefscdata/EDAB_Datasets/OISST/V2/SOURCE/SST" "//nefscdata/EDAB_Dev/atyrell" "//nefscdata/EDAB_Datasets/OISST/V2/SOURCE/SST_LTM/sst.day.mean.ltm.1991-2020.nc"
+
+# Command Line Cloud
+#Rscript ~/EDAB_Workflows/GLORYS_automation_test.R "~/EDAB_Datasets/OISST/V2/SOURCE/SST" "~/EDAB_Dev/atyrell" "~/EDAB_Datasets/OISST/V2/SOURCE/SST_LTM/sst.day.mean.ltm.1991-2020.nc"
 
 #Gets arguments from command line
 args = commandArgs(trailingOnly = TRUE)
@@ -7,11 +10,18 @@ if (length(args) > 0) {
   print(args)
   input_folder = args[1]
   output_folder = args[2]
+  ltm_file = args[3]
   print('Using command line arguments')
 } else {
-  input_folder = '//nefscdata/EDAB_Datasets/OISST/V2/SOURCE/SST'
-  output_folder = '//nefscdata/EDAB_Dev/atyrell'
-  print('Using default arguments')
+  # input_folder = '//nefscdata/EDAB_Datasets/OISST/V2/SOURCE/SST'
+  # output_folder = '//nefscdata/EDAB_Dev/atyrell'
+  # ltm_file = '//nefscdata/EDAB_Datasets/OISST/V2/SOURCE/SST_LTM/sst.day.mean.ltm.1991-2020.nc'
+
+  input_folder = '~/EDAB_Datasets/OISST/V2/SOURCE/SST'
+  output_folder = '~/EDAB_Dev/atyrell'
+  ltm_file = '~/EDAB_Datasets/OISST/V2/SOURCE/SST_LTM/sst.day.mean.ltm.1991-2020.nc'
+
+  message('Using default arguments')
 }
 
 message(paste0('input_folder: ', input_folder))
@@ -39,7 +49,7 @@ annual_mean <- EDABUtilities::make_2d_summary_ts(
   data.in = EDABUtilities::convert_2d_longitude_gridded(list.files(
     input_folder,
     full.names = TRUE
-  )),
+  )[1:3]),
   file.time = 'annual',
   output.files = NULL,
   shp.file = system.file(
@@ -53,16 +63,19 @@ annual_mean <- EDABUtilities::make_2d_summary_ts(
   tz = NA,
   touches = TRUE,
   write.out = F
-) |>
-  terra::as.data.frame(na.rm = FALSE)
+)
+
+annual_mean_output <- purrr::reduce(annual_mean,
+                                    ~ {terra::as.data.frame(.x, na.rm = FALSE)
+                                      dplyr::bind_rows(.x, .y)}
+                                    )
+
 
 # (2) calculate climatology
 message("Finished calculating annual means. Calculating climatology...")
 climatology <- EDABUtilities::make_2d_summary_ts(
   agg.time = "days",
-  data.in = EDABUtilities::convert_2d_longitude_gridded(
-    '//nefscdata/EDAB_Datasets/OISST/V2/SOURCE/SST_LTM/sst.day.mean.ltm.1991-2020.nc'
-  ),
+  data.in = EDABUtilities::convert_2d_longitude_gridded(ltm_file),
   file.time = 'annual',
   output.files = NULL,
   shp.file = system.file(
@@ -87,7 +100,7 @@ anomaly <- dplyr::full_join(
     dplyr::mutate(month = lubridate::month(time), day = lubridate::day(time)) |>
     dplyr::rename(climatology = value) |>
     dplyr::select(-time),
-  annual_mean |>
+  annual_mean_output |>
     dplyr::select(time, area, value) |>
     dplyr::mutate(
       month = lubridate::month(time),
@@ -95,8 +108,12 @@ anomaly <- dplyr::full_join(
       year = lubridate::year(time)
     )
 ) |>
-  dplyr::mutate(anom_value = value = climatology) |>
-  dplyr::group_by(year, month, area) |>
+  dplyr::mutate(anom_value = value - climatology,
+                season = dplyr::case_when(month %in% 1:3 ~ "Winter",
+                                          month %in% 4:6 ~ "Spring",
+                                          month %in% 7:9 ~ "Summer",
+                                          month %in% 10:12 ~ "Fall")) |>
+  dplyr::group_by(year, season, area) |>
   dplyr::summarise(data_value = mean(value, na.rm = TRUE))
 
 # (4) save to network drive
